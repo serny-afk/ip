8c6aa5412eb4d50945357cb50a6c93c24ea3ad0d
serny-afk <sernyuanong@gmail.com>
2026-01-16 17:40:48 +0800

    Skeletal Version

---
+++ src/main/java/Anoop.java
@@ -0,0 +1,11 @@
+public class Anoop {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+
+        System.out.println(line + "\nHello, I'm Anoop. I'm taking 48 MCs this sem.");
+        System.out.println("\nWhat can I do for you?\n" + line);
+        System.out.println("\nBye! See you when I graduate this sem!\n" + line);
+
+
+    }
+}
